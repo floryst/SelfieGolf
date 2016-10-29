c4946e61b5e1eb04658925ee19102c248dbd629e
Forrest <f@forrestli.com>
2016-10-29 14:56:08 -0400

    added gyro

--- SelfieGolf/www/js/index.js
+++ SelfieGolf/www/js/index.js
@@ -1,3 +1,12 @@
+// debug purposes only
+function dump(obj) {
+    var str = "";
+    for (var prop in obj) {
+        str += prop + "     ";
+    }
+    return str;
+}
+
 var app = {
     initialize: function() {
         this.bindEvents();
@@ -8,6 +17,7 @@ var app = {
     onDeviceReady: function() {
         document.getElementById('serverConnect')
             .addEventListener('click', app.onServerConnect, false);
+        console.log('ready');
     },
     onServerConnect: function(ev) {
         app.conn = new autobahn.Connection({
@@ -15,21 +25,32 @@ var app = {
             realm: 'golf_course'
         });
 
+        console.log('go connect');
         app.conn.onopen = app.onConnEstablished;
+
+        app.conn.open();
     },
-    onConnEstablished: function() {
+    onConnEstablished: function(session, details) {
+        console.log('connected');
+        app.session = session;
+
         app.accelWatchID = navigator.accelerometer.watchAcceleration(
                 app.onAccelSuccess, app.onAccelError, {
-                    frequency: 40 // 40ms, minimum iOS freq
+                    frequency: 20 // 40ms, minimum iOS freq
                 });
         app.compassWatchID = navigator.compass.watchHeading(
                 app.onCompassSuccess, app.onAccelError, {
-                    frequency: 40
+                    frequency: 20
+                });
+        app.gyroWatchID = navigator.gyroscope.watch(
+                app.onGyroSuccess, app.onAccelError, {
+                    frequency: 20
                 });
 
         document.addEventListener("volumeupbutton", app.onVolumeUp, false);
 
         document.body.setAttribute('style', 'background-color:green');
+        alert("done");
     },
     onVolumeUp: function(ev) {
         document.body.setAttribute('style', 'background-color:red');
@@ -40,6 +61,7 @@ var app = {
         document.getElementById('z').innerHTML = acc.z;
         // doesn't work?
         //document.getElementById('timestamp').innerHTML = acc.timestamp.toLocaleString();
+        app.session.publish('com.forrestli.selfiegolf.pubsub.accel', [acc.x, acc.y, acc.z]);
     },
     onAccelError: function() {
         alert('Error!');
@@ -47,5 +69,13 @@ var app = {
     onCompassSuccess: function(heading) {
         document.getElementById('magheading').innerHTML = heading.magneticHeading;
         document.getElementById('trueheading').innerHTML = heading.trueHeading;
+        app.session.publish('com.forrestli.selfiegolf.pubsub.orient',
+            [heading.magneticHeading, heading.trueHeading]);
+    },
+    onGyroSuccess: function(speed) {
+        document.getElementById('angx').innerHTML = speed.x;
+        document.getElementById('angy').innerHTML = speed.y;
+        document.getElementById('angz').innerHTML = speed.z;
+        app.session.publish('com.forrestli.selfiegolf.pubsub.gyro', [speed.x, speed.y, speed.z]);
     }
 };
