ccf75b84e4521815bf66304a81263a42bfbe4d57
Hastings Greer <hastings.greer@gmail.com>
2016-10-29 15:15:32 -0400

    textures

--- renderer/cube.js
+++ renderer/cube.js
@@ -11,8 +11,8 @@ function init() {
 
     scene = new THREE.Scene();
 
-    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
-    camera.position.z = 1000;
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, .1, 1000 );
+    camera.position.z = 4;
 
     geometry = new THREE.BoxGeometry( 200, 200, 200 );
     material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
@@ -20,15 +20,29 @@ function init() {
     mesh = new THREE.Mesh( geometry, material );
     scene.add( mesh );
 
+    var loader = new THREE.ColladaLoader();
+    loader.load('course.dae', function (result) {
+        scene.add(result.scene);
+        console.log("loader worked i guess");
+    });
+
+    var dirLight = new THREE.DirectionalLight(0xffffff, 1);
+    dirLight.position.set(100, 100, 50);
+    scene.add(dirLight);
+
+    var ambLight = new THREE.AmbientLight(0xffffff, .1);
+    scene.add(ambLight);
+
     renderer = new THREE.WebGLRenderer();
     renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setClearColor( 0xffffff, 1 );
 
     document.body.appendChild( renderer.domElement );
 
 }
 
 function animate() {
-    console.log("hi");
+
 
     requestAnimationFrame( animate );
 
