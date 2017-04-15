73dcbb8e53cdcce8ea013dbc5e28d6e120345e90
Hastings Greer <hastings.greer@kitware.com>
2017-04-15 14:09:52 -0400

    make multiplayer

--- server/golf.py
+++ server/golf.py
@@ -13,6 +13,9 @@ import collision.collision
 
 import numpy as np
 
+ids = set()
+
+
 # (sighs)
 ## LEVEL DATA
 # holes[i-1] is the (x,z) of ith hole
@@ -59,7 +62,47 @@ class Golf(ApplicationSession):
                 'com.forrestli.selfiegolf.pubsub.gyro')
         yield self.subscribe(self.onBoop,
                 'com.forrestli.selfiegolf.pubsub.boop')
+        yield self.register(self.newGame, 
+	        'com.forrestli.selfiegolf.pubsub.newGame')
+	yield self.register(self.endGame,
+	        'com.forrestli.selfiegolf.pubsub.endGame')
+	self.games = {}
 
+    def newGame(self, new_id):
+        if new_id not in self.games:
+	    self.games[new_id] = GolfGame(new_id, self)
+	else:
+	    return "no sorry someone else has that"
+    
+    def endGame(self, dead_id):
+        if dead_id in self.games:
+	    del self.games[dead_id]
+	else:
+	    return "That is not dead which can eternal lie, and with strange aeons even death may die"
+
+    def onBoop(self, my_id):
+        if my_id in self.games:
+            self.games[my_id].onBoop()
+
+    def onAccel(self, x, y, z, my_id):
+        if my_id in self.games:
+            self.games[my_id].onAccel(x, y, z)
+
+    def onOrient(self, mag, true, my_id):
+        if my_id in self.games:
+            self.games[my_id].onOrient(mag, true)
+
+    def onGyro(self, x, y, zi, my_id):
+        if my_id in self.games:
+            self.games[my_id].onGyro(x, y, zi)
+
+
+class GolfGame:
+
+    
+    def __init__(self, my_id, session):
+        self.my_id = my_id
+	self.session = session
         #game state
         self.stationary = True
         self.about2hit = False
@@ -76,15 +119,15 @@ class Golf(ApplicationSession):
     @inlineCallbacks
     def swing(self):
         self.strokes += 1
-        yield self.publish('com.forrestli.selfiegolf.pubsub.strokes', self.strokes)
+        yield self.session.publish('com.forrestli.selfiegolf.pubsub.strokes', self.strokes, self.my_id)
 
         v = self.prevDtheta * r
         path = collision.collision.path(self.x, self.z, v * np.cos(self.orientation), v * np.sin(self.orientation))
         for x, z in path:
             if z > self.hole_z-hole_halfwidth  and z < self.hole_z+hole_halfwidth and \
                     x > self.hole_x-hole_halfwidth and x < self.hole_x+hole_halfwidth:
-                subprocess.Popen(['play', '-q', '../golf_hole.py'])
-                yield self.call('com.forrestli.selfiegolf.hide_ball')
+                #subprocess.Popen(['play', '-q', '../golf_hole.py'])
+                yield self.session.publish('com.forrestli.selfiegolf.hide_ball', self.my_id)
                 # don't continue to perform swing
                 self.disable_hits = True
 
@@ -96,25 +139,25 @@ class Golf(ApplicationSession):
                 self.hole_x, self.hole_z = holes[self.cur_level]
                 self.x, self.z = start[self.cur_level]
                 self.stationary = True
-                yield self.publish('com.forrestli.selfiegolf.pubsub.ball', self.x, .1, self.z, self.stationary)
+                yield self.session.publish('com.forrestli.selfiegolf.pubsub.ball', self.x, .1, self.z, self.stationary, self.my_id)
 
                 self.strokes = 0
-                yield self.publish('com.forrestli.selfiegolf.pubsub.strokes', self.strokes)
+                yield self.session.publish('com.forrestli.selfiegolf.pubsub.strokes', self.strokes, self.my_id)
 
                 # stupid "mutex"
                 self.disable_hits = False
 
-                yield self.call('com.forrestli.selfiegolf.show_ball')
+                yield self.session.publish('com.forrestli.selfiegolf.show_ball', self.my_id)
 
                 return
-            yield self.publish('com.forrestli.selfiegolf.pubsub.ball', x, .1, z, self.stationary)
+            yield self.session.publish('com.forrestli.selfiegolf.pubsub.ball', x, .1, z, self.stationary, self.my_id)
             yield sleep(.01)
         self.stationary = True
         self.x = x
         self.z = z
-        yield self.publish('com.forrestli.selfiegolf.pubsub.ball', x, .1, z, self.stationary)
+        yield self.session.publish('com.forrestli.selfiegolf.pubsub.ball', x, .1, z, self.stationary, self.my_id)
 
-    def onBoop(self, msg):
+    def onBoop(self):
         if not self.disable_hits:
             self.log.info("boop")
             self.about2hit = True
@@ -136,7 +179,7 @@ class Golf(ApplicationSession):
         self.theta += x/dt
         self.DDtheta = (x - self.prevDtheta)/dt
         self.prevDtheta = x
-        yield self.publish('com.forrestli.selfiegolf.pubsub.orientation', self.orientation)
+        yield self.session.publish('com.forrestli.selfiegolf.pubsub.orientation', self.orientation, self.my_id)
         if(self.theta > 0 and self.about2hit):
             self.stationary = False
             self.about2hit = False
